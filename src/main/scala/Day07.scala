a1b7f1a773c7cc99db835f258df85ab1bd9363cf
Vincent Debergue <v.debergue@samsung.com>
2019-12-09 18:13:27 +0100

    :sparkles: Add Day07 P2

--- src/main/scala/Day07.scala
+++ src/main/scala/Day07.scala
@@ -1,4 +1,4 @@
-import Day05.MemoryState
+import Day05.{MemoryState, parseInstruction}
 
 object Day07 {
 
@@ -9,6 +9,15 @@ object Day07 {
 
     val best = findBestOutput(5, program)
     println(s"Best sequence output is $best")
+
+    val feedback = findBestFeedbackLoop(program)
+    println(s"f = $feedback")
+  }
+
+  def findBestFeedbackLoop(program: Seq[Int]): Int = {
+    val ampls = 5 to 9
+    val permutations = ampls.permutations
+    permutations.map(s => feedbackLoop(s, program)).max
   }
 
   def findBestOutput(numAmpli: Int, program: Seq[Int]): Int = {
@@ -28,5 +37,40 @@ object Day07 {
     val state = MemoryState(prog, pointer = 0, input = List(phaseSetting, inputSignal))
     val finalState = Day05.evaluateProgram(state)
     finalState.output.get
+  }
+
+  def feedbackLoop(settings: Seq[Int], program: Seq[Int]): Int = {
+    val states = settings.map(s => MemoryState(program.toArray, input = List(s), pointer = 0))
+    var signal = 0
+    var amplifierIndex = 0
+    var finished = false
+    while(!finished) {
+      val amplifier = states(amplifierIndex)
+      amplifier.input = amplifier.input :+ signal
+      var amplifierFinished = false
+
+      while(!amplifierFinished) {
+        val instruction = Day05.parseInstruction(amplifier.memory(amplifier.pointer))
+        val operation = Day05.Operation.toOperation(instruction.opCode)
+        val previousPointer = amplifier.pointer
+        operation.execute(instruction)(amplifier)
+        if (operation == Day05.Operation.SetOutput) {
+          signal = amplifier.output.get
+          amplifierFinished = true
+        }
+        if (amplifier.finished) {
+          amplifierFinished = true
+        }
+        if (previousPointer == amplifier.pointer) {
+          // advance pointer
+          amplifier.pointer = amplifier.pointer + operation.numParameters + 1
+        }
+      }
+      if (amplifierIndex == states.length - 1 && amplifier.finished) {
+        finished = true
+      }
+      amplifierIndex = (amplifierIndex + 1) % states.size
+    }
+    signal
   }
 }
--- src/test/scala/Day07Test.scala
+++ src/test/scala/Day07Test.scala
@@ -8,4 +8,10 @@ class Day07Test extends FunSuite {
     Day07.runSettings(Seq(4,3,2,1,0), input) should equal (43210)
     Day07.findBestOutput(5, input) should equal (43210)
   }
+
+  test("feedbackLoop") {
+    println("----------------------")
+    val input = Seq(3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5)
+    Day07.feedbackLoop(Seq(9,8,7,6,5), input) should equal (139629729)
+  }
 }
